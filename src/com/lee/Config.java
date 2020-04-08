8165077d01b38526d4ed610d27be0d3c128e30c2
leeyangT <ly125560278@163.com>
2020-04-08 20:50:57 +0800

    first commit

---
+++ src/com/lee/Config.java
@@ -0,0 +1,8 @@
+package com.lee;
+
+public class Config {
+    public static String db_host = "127.0.0.1";
+    public static String db_name = "darksiders";
+    public static String db_user = "root";
+    public static String db_pwd = "123";
+}
---
+++ src/com/lee/SidersDb.java
@@ -0,0 +1,41 @@
+package com.lee;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Statement;
+
+public class SidersDb {
+    private ComboPooledDataSource dataSource;
+
+    public void init(){
+        try {
+            dataSource = new ComboPooledDataSource();
+            dataSource.setJdbcUrl("jdbc:mysql://" + Config.db_host + ":3306/"+ Config.db_name +"?characterEncoding=utf-8");
+            dataSource.setDriverClass("com.mysql.jdbc.Driver");
+            dataSource.setUser(Config.db_user);
+            dataSource.setPassword(Config.db_pwd);
+            dataSource.setCheckoutTimeout(20000);       //获取一个connection超时时间，单位毫秒
+            dataSource.setInitialPoolSize(10);          //初始化时获取连接数，取值应在minPoolSize与maxPoolSize之间。Default: 3
+            dataSource.setAcquireIncrement(20);         //当连接池中的连接耗尽的时候c3p0一次同时获取的连接数。Default: 3
+            dataSource.setMinPoolSize(3);               //连接池中保留的最小连接数，默认为：3
+            dataSource.setMaxPoolSize(120);             //接池中保留的最大连接数。默认值: 15
+            dataSource.setMaxStatements(3000);          //c3p0全局的PreparedStatements缓存的大小。
+            //如果maxStatements与maxStatementsPerConnection均为0，则缓存不生效，只要有一个不为0，则语句的缓存就能生效。如果默认值: 0
+            dataSource.setMaxIdleTime(3600);            //最大空闲时间，多少秒内未使用则连接被丢弃。若为0则永不丢弃。默认值: 0
+            dataSource.setIdleConnectionTestPeriod(10); //每隔多少秒检查所有连接池中的空闲连接。Default: 0
+            dataSource.setAcquireRetryAttempts(3000);   //重新尝试的时间间隔，默认为：1000毫秒
+
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            dataSource = null;
+        }
+    }
+
+    public Statement getStatement(){
+
+    }
+
+
+}
